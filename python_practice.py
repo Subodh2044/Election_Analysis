f50fedd5bddd944289d5a8783b03b79118bb4c94
Subodh44 <byansubodh@gmail.com>
2020-09-16 21:41:18 -0500

    csv

---
+++ python_practice.py
@@ -0,0 +1,24 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == "Denver":
+    print(counties[1])
+
+
+temp = 80
+if temp > 80:
+    print("Turn on the AC")
+else:
+    print("Open the window")
+
+
+score = 25
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+else:
+    print("F")
+
+x = 0
+while   x < 5:
+    print(x)
+    x = x+1
